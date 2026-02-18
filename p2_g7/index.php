8b465c54ce3a9297d4ee0f857ec54e5e3d671541
Alee200210 <150607170+Alee200210@users.noreply.github.com>
2026-02-18 20:12:55 +0100

    Refactor includes to use absolute paths

--- p2_g7/index.php
+++ p2_g7/index.php
@@ -1,43 +1,33 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bistro FDI</title>
-</head>
-<body>
+<?php
+include __DIR__ . '/includes/views/partials/cabecera.php'; 
+include __DIR__ . '/includes/views/partials/sideBarIzq.php'; 
 
-    <?php include './includes/views/partials/cabecera.php'; ?>
+$contenido = '';
 
-    <?php include './includes/views/partials/sideBarIzq.php'; ?>
-
-    <?php
-        $pagina = $_GET['pagina'] ?? 'inicio';
+$pagina = $_GET['pagina'] ?? 'inicio';
         
-        switch ($pagina) {
-            case 'contacto':
-                require './includes/views/pages/contacto.php';
-                break;
-            case 'takeAway':
-                require './includes/views/pages/takeAway.php';
-                break;
-            case 'eatIn':
-                require './includes/views/pages/eatIn.php';
-                break;
-            default:
-                require './includes/views/pages/plantilla.php';
+switch ($pagina) {
+    case 'contacto':
+        require __DIR__ . '/includes/views/pages/contacto.php';
+        break;
+    case 'takeAway':
+        require __DIR__ . '/includes/views/pages/takeAway.php';
+        break;
+    case 'eatIn':
+        require __DIR__ . '/includes/views/pages/eatIn.php';
+        break;
+    default:
+        require __DIR__ . '/includes/views/pages/plantilla.php';
         }
-    ?>
+?>
     
-    <main>
-        <article>
-            <?php echo $contenido; ?>
-        </article>
-    </main>
+<main>
+    <article>
+        <?php echo $contenido; ?>
+    </article>
+</main>
 
-    <?php include './includes/views/partials/sideBarDer.php'; ?>
-    <?php include './includes/views/partials/pie.php'; ?>
-
-</body>
-</html>
+<?php 
+include __DIR__ . '/includes/views/partials/sideBarDer.php'; 
+include __DIR__ . '/includes/views/partials/pie.php'; 
+?>
