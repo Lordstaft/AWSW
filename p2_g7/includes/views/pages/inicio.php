09131e435bbe455dc9c917d3ea36371cffd213b0
Alee200210 <150607170+Alee200210@users.noreply.github.com>
2026-02-18 20:56:07 +0100

    Add files via upload

---
+++ p2_g7/includes/views/pages/inicio.php
@@ -0,0 +1,10 @@
+<?php
+$contenido = <<<EOS
+  <h1>Inicio</h1>
+  <p>Página principal (provisional).</p>
+  <ul>
+    <li><a href="index.php?pagina=login">Login</a></li>
+    <li><a href="index.php?pagina=registro">Registro</a></li>
+  </ul>
+EOS;
+
