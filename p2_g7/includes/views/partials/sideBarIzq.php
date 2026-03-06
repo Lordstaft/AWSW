f18fdf528e3d9f841f24619caa41b90c2a940b1b
Alee200210 <150607170+Alee200210@users.noreply.github.com>
2026-03-06 21:09:57 +0100

    Update sideBarIzq.php

--- p2_g7/includes/views/partials/sideBarIzq.php
+++ p2_g7/includes/views/partials/sideBarIzq.php
@@ -1,22 +1,37 @@
 <?php
+
 function mostrarAdministrar() {
-	if (isset($_SESSION['esAdmin']) && $_SESSION['esAdmin']===true){
-		return "<li><a href='/../pages/admin.php'>Administrar</a></li>";
-	}
-	    return "";
+    if (isset($_SESSION['esAdmin']) && $_SESSION['esAdmin'] === true) {
+        return "
+            <li><a href='" . RUTA_APP . "/includes/views/pages/admin.php'>Administrar</a></li>
+            <li><a href='" . RUTA_APP . "/includes/views/pages/productos.php'>Productos</a></li>
+            <li><a href='" . RUTA_APP . "/includes/views/pages/categorias.php'>Categorías</a></li>
+        ";
+    }
+    return "";
 }
+
 function mostrarNavegacion() {
-    if (isset($_SESSION['login']) && $_SESSION['login']===true) {
-        return "<li><a href='/ej2_p12/01-inicio/01-inicio/01-inicio/includes/views/plantillas/admin.php'>Administrar</a></li>";
-	}
-	return "";	
+    if (isset($_SESSION['login']) && $_SESSION['login'] === true) {
+        return "
+            <li><a href='" . RUTA_APP . "/index.php'>Inicio</a></li>
+            <li><a href='" . RUTA_APP . "/includes/views/pages/productos.php'>Ver productos</a></li>
+            <li><a href='" . RUTA_APP . "/includes/views/pages/categorias.php'>Ver categorías</a></li>
+	    <li><a href="< . RUTA_APP . /includes/views/pages/crearCategoria.php">Crear categoría</a></li>
+        ";
+    } else {
+        return "
+            <li><a href='" . RUTA_APP . "/index.php'>Inicio</a></li>
+            <li><a href='" . RUTA_APP . "/includes/views/pages/registro.php'>Registro</a></li>
+        ";
+    }
 }
 ?>
 
 <nav id="sidebarIzq">
-	<h3>Navegación</h3>
-	<ul>
-        <?= mostrarAdministrar(); ?>
+    <h3>Navegación</h3>
+    <ul>
         <?= mostrarNavegacion(); ?>
-	</ul>
+        <?= mostrarAdministrar(); ?>
+    </ul>
 </nav>
