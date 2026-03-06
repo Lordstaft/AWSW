d167a0c89e1562f3f958e8543d0f02423ee4bedc
Alee200210 <150607170+Alee200210@users.noreply.github.com>
2026-03-06 20:09:02 +0100

    Add files via upload

---
+++ p2_g7/includes/views/pages/categorias.php
@@ -0,0 +1,46 @@
+<?php
+require __DIR__ . '/../../config.php';
+
+$categorias = Categoria::listar();
+
+$filas = '';
+
+foreach ($categorias as $categoria) {
+
+	if (!empty($categoria['imgCategoriaProd'])) {
+		$rutaImagen = RUTA_IMGS . '/' . $categoria['imgCategoriaProd'];
+		$imagen = "<img src=\"$rutaImagen\" alt=\"{$categoria['nombre']}\" width=\"100\">";
+	} else {
+		$imagen = 'Sin imagen';
+	}
+    $descripcion = $categoria['descripcion'] ? $categoria['descripcion'] : 'Sin descripción';
+
+    $filas .= "<tr>
+        <td>{$categoria['id']}</td>
+        <td>{$categoria['nombre']}</td>
+        <td>{$descripcion}</td>
+        <td>{$imagen}</td>
+    </tr>";
+}
+
+$tituloPagina = 'Categorías';
+
+$contenidoPrincipal = <<<EOS
+<h1>Listado de categorías</h1>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Imagen</th>
+        </tr>
+    </thead>
+    <tbody>
+        $filas
+    </tbody>
+</table>
+EOS;
+
+require __DIR__ . '/plantilla.php';
