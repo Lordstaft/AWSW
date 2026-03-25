a1a473bc53f6528995d9b8bb96361f84d8fb2c80
Alee200210 <150607170+Alee200210@users.noreply.github.com>
2026-03-25 19:55:34 +0100

    Add files via upload

---
+++ p3_g7/inicio.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . '/../../config.php';
+
+$tituloPagina = 'Inicio';
+
+$contenidoPrincipal = <<<EOS
+  <a href="./pedido.php?pagina=takeAway">Pedido para llevar</a>
+  <a href="./pedido.php?pagina=eatIn">Para consumir en el local</a>
+EOS;
+
+require __DIR__ . '/plantilla.php';
