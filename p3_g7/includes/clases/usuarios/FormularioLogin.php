bdc961a1edb69c16221afa1f725414685eaee507
Alee200210 <150607170+Alee200210@users.noreply.github.com>
2026-03-25 19:31:14 +0100

    Add files via upload

--- p3_g7/includes/clases/usuarios/FormularioLogin.php
+++ p3_g7/includes/clases/usuarios/FormularioLogin.php
@@ -1,77 +1,77 @@
 <?php
 namespace es\ucm\fdi\aw\usuarios;
+
 use es\ucm\fdi\aw\Formulario;
-use es\ucm\fdi\aw\usuarios\Usuario;
-use es\ucm\fdi\aw\usuarios\Roles;
 
 class FormularioLogin extends Formulario
 {
     public function __construct() {
-        parent::__construct('formLogin', ['urlRedireccion' => 'index.php']);
+        parent::__construct('formLogin');
     }
-    
+
     protected function generaCamposFormulario(&$datos)
     {
         $nombreUsuario = $datos['nombreUsuario'] ?? '';
         $password = $datos['password'] ?? '';
 
-        $htmlErroresGlobales = self::generaListaErroresGlobales($this->errores);
-        $erroresCampos = self::generaErroresCampos(['nombreUsuario', 'password'], $this->errores, 'span', array('class' => 'error'));
-
-        $html = <<<EOF
-        $htmlErroresGlobales
+        $html = <<<EOS
         <fieldset>
-            <legend>Usuario y contraseña</legend>
+            <legend>Login</legend>
             <div>
                 <label for="nombreUsuario">Nombre de usuario:</label>
                 <input id="nombreUsuario" type="text" name="nombreUsuario" value="$nombreUsuario" />
-                {$erroresCampos['nombreUsuario']}
             </div>
             <div>
-                <label for="password">Password:</label>
+                <label for="password">Contraseña:</label>
                 <input id="password" type="password" name="password" value="$password" />
-                {$erroresCampos['password']}
             </div>
             <div>
                 <button type="submit" name="login">Entrar</button>
             </div>
         </fieldset>
-        EOF;
+        EOS;
+
         return $html;
     }
 
     protected function procesaFormulario(&$datos)
     {
         $this->errores = [];
+
         $nombreUsuario = trim($datos['nombreUsuario'] ?? '');
-        $nombreUsuario = filter_var($nombreUsuario, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        if ( ! $nombreUsuario || empty($nombreUsuario) ) {
+        $password = trim($datos['password'] ?? '');
+
+        if ($nombreUsuario === '') {
             $this->errores['nombreUsuario'] = 'El nombre de usuario no puede estar vacío';
         }
-        
-        $password = trim($datos['password'] ?? '');
-        $password = filter_var($password, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        if ( ! $password || empty($password) ) {
-            $this->errores['password'] = 'El password no puede estar vacío.';
+
+        if ($password === '') {
+            $this->errores['password'] = 'La contraseña no puede estar vacía';
         }
-        
+
         if (count($this->errores) === 0) {
             $usuario = Usuario::login($nombreUsuario, $password);
-        
+
             if (!$usuario) {
                 $this->errores[] = "El usuario o el password no coinciden";
-            } else {
+            }
+            else {
                 $_SESSION['login'] = true;
+                $_SESSION['idUsuario'] = $usuario->getId();
                 $_SESSION['nombre'] = $usuario->getNombre();
                 $_SESSION['nombreUsuario'] = $usuario->getNombreUsuario();
-                if($usuario->getRol() === Roles::ADMIN->value) {
+                $_SESSION['rol'] = $usuario->getRol();
+                $_SESSION['roles'] = [$usuario->getRol()];
+
+                if ($usuario->getRol() === Roles::ADMIN) {
                     $_SESSION['esAdmin'] = true;
                     $_SESSION['esGerente'] = true;
                 }
-                elseif($usuario->getRol() === Roles::GERENTE->value){
+                elseif ($usuario->getRol() === Roles::GERENTE) {
+                    $_SESSION['esAdmin'] = false;
                     $_SESSION['esGerente'] = true;
                 }
-                else{
+                else {
                     $_SESSION['esAdmin'] = false;
                     $_SESSION['esGerente'] = false;
                 }
