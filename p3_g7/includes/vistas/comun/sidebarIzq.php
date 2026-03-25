ab5b0d6fa688b4ee61f560520cb36c4233c3019b
Alee200210 <150607170+Alee200210@users.noreply.github.com>
2026-03-25 16:20:53 +0100

    Añadido panel de cocina y pedidos gerente

--- p3_g7/includes/vistas/comun/sidebarIzq.php
+++ p3_g7/includes/vistas/comun/sidebarIzq.php
@@ -9,5 +9,8 @@ $app = Aplicacion::getInstance();
 		<li><a href="<?= $app->resuelve('/index.php')?>">Inicio</a></li>
 		<li><a href="<?= $app->resuelve('/contenido.php')?>">Ver contenido</a></li>
 		<li><a href="<?= $app->resuelve('/admin.php')?>">Administrar</a></li>
+		<li><a href="<?= $app->resuelve('/cocina.php')?>">Panel cocina</a></li>
+		<li><a href="<?= $app->resuelve('/pedidosGerente.php')?>">Pedidos gerente</a></li>
 	</ul>
 </nav>
+
