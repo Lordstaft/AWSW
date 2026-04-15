b258a1772783fe4c62c220b30e8786e5b78d1e6b
Alee200210 <150607170+Alee200210@users.noreply.github.com>
2026-04-15 19:32:49 +0200

    Update plantilla.php

--- p3_g7/includes/vistas/plantillas/plantilla.php
+++ p3_g7/includes/vistas/plantillas/plantilla.php
@@ -3,11 +3,12 @@ $params['app']->doInclude('/vistas/helpers/plantilla.php');
 $mensajes = mensajesPeticionAnterior();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
 	<meta charset="UTF-8">
     <title><?= $params['tituloPagina'] ?></title>
-	<link rel="stylesheet" type="text/css" href="<?= $params['app']->resuelve('/css/estilo.css') ?>" /></head>
+	<link rel="stylesheet" type="text/css" href="<?= $params['app']->resuelve('/css/estilo.css') ?>">
+</head>
 <body>
 <?= $mensajes ?>
 <div id="contenedor">
