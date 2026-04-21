60e663a17214608e58be70782df3151398421bb2
Alee200210 <150607170+Alee200210@users.noreply.github.com>
2026-04-21 10:38:59 +0200

    Add files via upload

--- p3_g7/index.php
+++ p3_g7/index.php
@@ -1,5 +1,20 @@
 <?php
 
 require_once __DIR__.'/includes/config.php';
+use \es\ucm\fdi\aw\usuarios\FormularioLogin;
 
-$app->redirige('inicio.php');
+$formLogin = new FormularioLogin();
+$formLogin = $formLogin->gestiona();
+
+if (isset($_SESSION['login']) && $_SESSION['login'] === true) {
+    $app->redirige($app->resuelve('/inicio.php'));
+}
+
+$tituloPagina = 'Login';
+$contenidoPrincipal = <<<EOF
+    <h1>Acceso al sistema</h1>
+    $formLogin
+EOF;
+
+$params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Login'];
+$app->generaVista('/plantillas/plantilla.php', $params);
