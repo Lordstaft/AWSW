7106541c70e96c7c55c71df0bc9328abed837853
pablsa18 <154286687+pablsa18@users.noreply.github.com>
2026-05-01 18:21:21 +0200

    Add files via upload

--- proyecto_g7/usuarios/admin.php
+++ proyecto_g7/usuarios/admin.php
@@ -3,13 +3,22 @@ require __DIR__ . '/../includes/config.php';
 use es\ucm\fdi\aw\usuarios\FormularioBusquedaUsuarios;
 
 $tituloPagina = 'Buscar usuario';
-$formulario = new FormularioBusquedaUsuarios();
-$formularioHTML = $formulario->gestiona();
 
-$contenidoPrincipal = <<<EOS
-    <h2>Buscar Usuario</h2>
-    $formularioHTML
-EOS;
+if (isset($_SESSION["esAdmin"])) {
+    $formulario = new FormularioBusquedaUsuarios();
+    $formularioHTML = $formulario->gestiona();
+
+    $contenidoPrincipal = <<<EOS
+        <h1>Buscar Usuario</h1>
+        $formularioHTML
+    EOS;
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión como administrador para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Panel Administrador'];
 $app->generaVista('/plantillas/plantilla.php', $params);
--- proyecto_g7/usuarios/perfil.php
+++ proyecto_g7/usuarios/perfil.php
@@ -2,14 +2,23 @@
 require __DIR__ . '/../includes/config.php';
 use es\ucm\fdi\aw\usuarios\FormularioPerfil;
 
-$tituloPagina = 'Buscar usuario';
-$formulario = new FormularioPerfil();
-$formularioHTML = $formulario->gestiona();
+$tituloPagina = 'Ver perfil';
 
-$contenidoPrincipal = <<<EOS
-    <h2>Perfil</h2>
-    $formularioHTML
-EOS;
+if (isset($_SESSION["login"])) {
+    $formulario = new FormularioPerfil();
+    $formularioHTML = $formulario->gestiona();
+
+    $contenidoPrincipal = <<<EOS
+        <h1>Perfil</h1>
+        $formularioHTML
+    EOS;
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Bistro FDI'];
 $app->generaVista('/plantillas/plantilla.php', $params);
