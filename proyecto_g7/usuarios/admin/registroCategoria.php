479e9ba044f42b80f7882a7dfd05e7225c8f7d42
pablsa18 <154286687+pablsa18@users.noreply.github.com>
2026-05-01 19:51:01 +0200

    Add files via upload

--- proyecto_g7/usuarios/admin/registroCategoria.php
+++ proyecto_g7/usuarios/admin/registroCategoria.php
@@ -2,14 +2,23 @@
 require __DIR__ . '/../../includes/config.php';
 use es\ucm\fdi\aw\productos\FormularioCrearCategoria;
 
-$tituloPagina = 'Registro categoria';
-$formulario = new FormularioCrearCategoria();
-$formularioHTML = $formulario->gestiona();
+$tituloPagina = 'Crear Categoría';
 
-$contenidoPrincipal = <<<EOS
-    <h2>Crear categoria</h2>
-    $formularioHTML
-EOS;
+if (isset($_SESSION["esAdmin"])) {
+    $formulario = new FormularioCrearCategoria();
+    $formularioHTML = $formulario->gestiona();
+
+    $contenidoPrincipal = <<<EOS
+        <h1>Crear categoría</h1>
+        $formularioHTML
+    EOS;
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión como administrador para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Panel Administrador'];
 $app->generaVista('/plantillas/plantilla.php', $params);
