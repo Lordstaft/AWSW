057cbc816a758723164cd4f5bc0d2c854c936679
pablsa18 <154286687+pablsa18@users.noreply.github.com>
2026-05-01 18:22:12 +0200

    Add files via upload

--- proyecto_g7/usuarios/camarero/gestionarEntregas.php
+++ proyecto_g7/usuarios/camarero/gestionarEntregas.php
@@ -1,18 +1,24 @@
 <?php
 require __DIR__ . '/../../includes/config.php';
-
 use es\ucm\fdi\aw\pedidos\FormularioEntregaPedido;
 
 $tituloPagina = 'Entrega de Pedidos';
 
-$formulario = new FormularioEntregaPedido();
-$formularioHTML = $formulario->gestiona();
+if (isset($_SESSION["esCamarero"])) {
+    $formulario = new FormularioEntregaPedido();
+    $formularioHTML = $formulario->gestiona();
 
-
-$contenidoPrincipal = <<<EOS
-    <h2>Entrega de Pedidos</h2>
-    $formularioHTML
-EOS;
+    $contenidoPrincipal = <<<EOS
+        <h1>Entrega de pedidos</h1>
+        $formularioHTML
+    EOS;
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión como camarero para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Bistro FDI'];
 $app->generaVista('/plantillas/plantilla.php', $params);
