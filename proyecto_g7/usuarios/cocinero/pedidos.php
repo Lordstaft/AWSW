98cc10c0445b73f2c99f1975fc5a3ac05eaff70c
pablsa18 <154286687+pablsa18@users.noreply.github.com>
2026-05-01 18:23:13 +0200

    Add files via upload

--- proyecto_g7/usuarios/cocinero/pedidos.php
+++ proyecto_g7/usuarios/cocinero/pedidos.php
@@ -1,20 +1,24 @@
 <?php
-//Inicio del procesamiento
 require __DIR__ . '/../../includes/config.php';
-
 use es\ucm\fdi\aw\pedidos\FormularioPedidosEnCocina;
 
 $tituloPagina = 'Pedidos sin cocinar';
 
-$formulario = new FormularioPedidosEnCocina();
-$formularioHTML = $formulario->gestiona();
+if (isset($_SESSION["esCocinero"])) {
+    $formulario = new FormularioPedidosEnCocina();
+    $formularioHTML = $formulario->gestiona();
 
-
-$contenidoPrincipal = <<<EOS
-    <h2>Resultados de la búsqueda</h2>
-    $formularioHTML
-EOS;
+    $contenidoPrincipal = <<<EOS
+        <h1>Resultados de la búsqueda</h1>
+        $formularioHTML
+    EOS;
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión como cocinero para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Bistro FDI'];
 $app->generaVista('/plantillas/plantilla.php', $params);
-
--- proyecto_g7/usuarios/cocinero/pedidosPendientes.php
+++ proyecto_g7/usuarios/cocinero/pedidosPendientes.php
@@ -1,19 +1,24 @@
 <?php
-//Inicio del procesamiento
 require __DIR__ . '/../../includes/config.php';
-
 use es\ucm\fdi\aw\pedidos\FormularioPedidosPendientes;
 
 $tituloPagina = 'Pedidos Pendientes';
-$formulario = new FormularioPedidosPendientes();
 
-$formularioHTML = $formulario->gestiona();
+if (isset($_SESSION["esCocinero"])) {
+    $formulario = new FormularioPedidosPendientes();
+    $formularioHTML = $formulario->gestiona();
 
-$contenidoPrincipal = <<<EOS
-    <h2>Pedidos Pendientes</h2>
-    $formularioHTML
-EOS;
+    $contenidoPrincipal = <<<EOS
+        <h1>Pedidos pendientes</h1>
+        $formularioHTML
+    EOS;
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión como cocinero para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Bistro FDI'];
 $app->generaVista('/plantillas/plantilla.php', $params);
-
