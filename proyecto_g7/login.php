b0e46d0ebc0c6199826ad233ced44fddace08c87
pablsa18 <154286687+pablsa18@users.noreply.github.com>
2026-05-01 18:19:48 +0200

    Add files via upload

--- proyecto_g7/login.php
+++ proyecto_g7/login.php
@@ -1,16 +1,24 @@
 <?php
-
 require_once __DIR__.'/includes/config.php';
 use \es\ucm\fdi\aw\usuarios\FormularioLogin;
 
-$formLogin = new FormularioLogin();
-$formLogin = $formLogin->gestiona();
-
 $tituloPagina = 'Login';
-$contenidoPrincipal=<<<EOF
-  	<h1>Acceso al sistema</h1>
-    $formLogin
-EOF;
+
+if (!isset($_SESSION["login"])) {
+  $formLogin = new FormularioLogin();
+  $formLogin = $formLogin->gestiona();
+
+  $contenidoPrincipal=<<<EOF
+      <h1>Acceso al sistema</h1>
+      $formLogin
+  EOF;
+}
+else {
+  $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Ya hay una sesión iniciada.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Login'];
 $app->generaVista('/plantillas/plantilla.php', $params);
