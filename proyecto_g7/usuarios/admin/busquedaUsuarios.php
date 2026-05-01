16500edab2766221b421b13fe90405ec4c83b9fd
pablsa18 <154286687+pablsa18@users.noreply.github.com>
2026-05-01 19:47:17 +0200

    Add files via upload

--- proyecto_g7/usuarios/admin/busquedaUsuarios.php
+++ proyecto_g7/usuarios/admin/busquedaUsuarios.php
@@ -1,22 +1,28 @@
 <?php
-//Inicio del procesamiento
 require __DIR__ . '/../../includes/config.php';
-
 use es\ucm\fdi\aw\usuarios\FormularioBusquedaUsuarios;
 
-$tituloPagina = 'Busqueda de usuarios';
+$tituloPagina = 'Búsqueda de Usuarios';
 
-$formulario = new FormularioBusquedaUsuarios();
-$formulario->gestiona();
+if (isset($_SESSION["esAdmin"])) {
+    $formulario = new FormularioBusquedaUsuarios();
+    $formulario->gestiona();
 
-$resultados = $_SESSION['resultadosBusqueda'];
+    $resultados = $_SESSION['resultadosBusqueda'];
 
-$contenidoPrincipal = <<<EOS
-    <h2>Resultados de la búsqueda</h2>
-    $resultados
-EOS;
+    $contenidoPrincipal = <<<EOS
+        <h1>Resultados de la búsqueda</h1>
+        $resultados
+    EOS;
 
-unset($_SESSION['resultadosBusqueda']);
+    unset($_SESSION['resultadosBusqueda']);
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión como administrador para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Panel Administrador'];
 $app->generaVista('/plantillas/plantilla.php', $params);
--- proyecto_g7/usuarios/admin/modificarUsuario.php
+++ proyecto_g7/usuarios/admin/modificarUsuario.php
@@ -1,18 +1,24 @@
 <?php
 require __DIR__ . '/../../includes/config.php';
-
 use es\ucm\fdi\aw\usuarios\FormularioEditarUsuario;
 
-$tituloPagina = 'Editar usuario';
+$tituloPagina = 'Editar Usuario';
 
-$formulario = new FormularioEditarUsuario();
-$formularioHTML = $formulario->gestiona();
+if (isset($_SESSION["esAdmin"])) {
+    $formulario = new FormularioEditarUsuario();
+    $formularioHTML = $formulario->gestiona();
 
-
-$contenidoPrincipal = <<<EOS
-    <h2>Editar usuario</h2>
-    $formularioHTML
-EOS;
+    $contenidoPrincipal = <<<EOS
+        <h1>Editar usuario</h1>
+        $formularioHTML
+    EOS;
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión como administrador para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Panel Administrador'];
 $app->generaVista('/plantillas/plantilla.php', $params);
