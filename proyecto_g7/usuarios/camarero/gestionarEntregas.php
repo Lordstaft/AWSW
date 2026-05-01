0cfa3ed4d43367ef20197eeb57f4e62487e8fe4c
pablsa18 <154286687+pablsa18@users.noreply.github.com>
2026-05-01 21:25:14 +0200

    Add files via upload

--- proyecto_g7/usuarios/camarero/gestionarEntregas.php
+++ proyecto_g7/usuarios/camarero/gestionarEntregas.php
@@ -9,13 +9,13 @@ if (isset($_SESSION["esCamarero"])) {
     $formularioHTML = $formulario->gestiona();
 
     $contenidoPrincipal = <<<EOS
-        <h1>Entrega de pedidos</h1>
+        <h2>Entrega de pedidos</h2>
         $formularioHTML
     EOS;
 }
 else {
     $contenidoPrincipal = <<<EOS
-        <h1>Acceso denegado</h1>
+        <h2>Acceso denegado</h2>
         <p>Debes iniciar sesión como camarero para ver el contenido.</p>
     EOS;
 }
