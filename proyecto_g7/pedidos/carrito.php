e3fc0e3ef44e6b8666962dfbfa8cf988882ae639
pablsa18 <154286687+pablsa18@users.noreply.github.com>
2026-05-01 19:31:30 +0200

    Add files via upload

--- proyecto_g7/pedidos/carrito.php
+++ proyecto_g7/pedidos/carrito.php
@@ -3,12 +3,21 @@ require __DIR__ . '/../includes/config.php';
 use es\ucm\fdi\aw\pedidos\FormularioCarrito;
 
 $tituloPagina = 'Carrito';
-$formulario = new FormularioCarrito();
-$formularioHTML = $formulario->gestiona();
 
-$contenidoPrincipal = <<<EOS
-    $formularioHTML
-EOS;
+if (isset($_SESSION["login"])) {
+    $formulario = new FormularioCarrito();
+    $formularioHTML = $formulario->gestiona();
+
+    $contenidoPrincipal = <<<EOS
+        $formularioHTML
+    EOS;
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Bistro FDI'];
 $app->generaVista('/plantillas/plantilla.php', $params);
--- proyecto_g7/pedidos/realizarPedido.php
+++ proyecto_g7/pedidos/realizarPedido.php
@@ -1,17 +1,23 @@
 <?php
 require __DIR__ . '/../includes/config.php';
-
 use es\ucm\fdi\aw\pedidos\FormularioRealizarPedido;
 
-$tituloPagina = 'Realizar pedido';
+$tituloPagina = 'Realizar Pedido';
 
-$formulario = new FormularioRealizarPedido();
-$formularioHTML = $formulario->gestiona();
+if (isset($_SESSION["login"])) {
+    $formulario = new FormularioRealizarPedido();
+    $formularioHTML = $formulario->gestiona();
 
-
-$contenidoPrincipal = <<<EOS
-    $formularioHTML
-EOS;
+    $contenidoPrincipal = <<<EOS
+        $formularioHTML
+    EOS;
+}
+else {
+    $contenidoPrincipal = <<<EOS
+        <h1>Acceso denegado</h1>
+        <p>Debes iniciar sesión para ver el contenido.</p>
+    EOS;
+}
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'cabecera' => 'Bistro FDI'];
 $app->generaVista('/plantillas/plantilla.php', $params);
